aea431dbdf3ffdc61ea032b819e02d374bdffc5f
Mustafa Ersoy <mustafaersoy@users.noreply.github.com>
2018-11-22 21:54:46 +0300

    Rest Api Added

---
+++ Rest Api/db.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "sosyalkitap";
+
+try {
+	$db = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+        $db->query("SET CHARACTER SET utf8");
+}catch(PDOException $e) {
+	die( $e->getMessage());
+}
+
+ ?>
---
+++ Rest Api/yazar.php
@@ -0,0 +1,43 @@
+<?php
+include "db.php";
+include "function.php";
+$islem = isset($_GET["islem"]) ? addslashes(trim($_GET["islem"])) : null;
+$jsonArray = array(); // array değişkenimiz bunu en alta json objesine çevireceğiz.
+$jsonArray["hata"] = FALSE; // Başlangıçta hata yok olarak kabul edelim.
+
+$_code = 200; // HTTP Ok olarak durumu kabul edelim.
+$_method = $_SERVER["REQUEST_METHOD"]; // client tarafından bize gelen method
+// aldığımız işlem değişkenine göre işlemler yapalım.
+  if($_method  == "POST") {
+  }
+  else if($_method == "PUT") {
+  }
+  else if($_method == "DELETE") {
+  }
+  else if($_method == "GET") {
+       $bilgiler = $db->query("SELECT yazarIsim from yazarlar", PDO::FETCH_ASSOC);
+       if($bilgiler->rowCount()>0) {
+         $_code = 200;
+         $i=1;
+         foreach( $bilgiler as $row ){
+            $jsonArray["bilgi".$i] = $row;
+            $i=$i+1;
+         }
+       }
+       else {
+        $_code = 400;
+        $jsonArray["hata"] = TRUE; // bir hata olduğu bildirilsin.
+        $jsonArray["hataMesaj"] = "Kayıt bulunamadı"; // Hatanın neden kaynaklı olduğu belirtilsin.
+       }
+    }
+
+  else {
+    // hatalı bir parametre girilmesi durumunda burası çalışacak.
+    $jsonArray["hata"] = TRUE; // bir hata olduğu bildirilsin.
+    $jsonArray["hataMesaj"] = "Girilen İşlem Bulunmuyor."; // Hatanın neden kaynaklı olduğu belirtilsin.
+  }
+
+SetHeader($_code);
+$jsonArray[$_code] = HttpStatus($_code);
+echo json_encode($jsonArray);
+?>
